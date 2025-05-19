3b04ac961d9a0cef5be446f72cbc5808145e3cae
백지원 Jiwon Baek <wldnjsl2001@khu.ac.kr>
2025-05-20 01:26:18 +0900

    Upgrade 오목.py
    https://www.acmicpc.net/problem/2615

--- qorjiwon/Week4/오목.py
+++ qorjiwon/Week4/오목.py
@@ -1,21 +1,32 @@
+#육목 조심
+
+# := 연산자 : "워러스 연산자" 또는 "대입 표현식 연산자"라고 불림. 
+# 변수에 값을 대입하면서 동시에 그 값을 표현식에 사용할 수 있도록 해준다.
+
 import sys
 input = sys.stdin.readline
 
-dx = [0, 1, 1, -1]
-dy = [1, 0, 1, 1]
+# 우, 하, 우하, 우상
+d = ((1, 0), (0, 1), (1, 1), (-1, 1))
 
 m = [list(map(int, input().rstrip().split())) for _ in range(19)]
 
 for i in range(19):
     for j in range(19):
-        if m[i][j]:
-            for k in range(4):
+        if s:=m[i][j]:
+            for dx, dy in d:
                 cnt = 1
-                for l in range(1, 6):
-                    nx = i + dx[k]*l
-                    ny = j + dy[k]*l
-                    if 0 <= nx < 19 and 0 <= ny < 19 and m[nx][ny] == m[i][j]:
-                        cnt += 1
+                nx = i + dx
+                ny = j + dy
+                while 0 <= nx < 19 and 0 <= ny < 19 and m[nx][ny] == s:
+                    cnt += 1
+                    nx += dx
+                    ny += dy
                 if cnt == 5:
+                    nx = i - dx
+                    ny = j - dy
+                    if 0 <= nx < 19 and 0 <= ny < 19 and m[nx][ny] == s: # 6목 확인
+                        continue
                     print(f'{m[i][j]}\n{i + 1} {j + 1}')
                     exit()
+print(0)
