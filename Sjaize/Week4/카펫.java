88aa141f559c4bc68e8859ba8b67f3f82c9d6b2b
JongHyun Oh <sjaiz1717@gmail.com>
2025-05-19 13:28:05 +0900

    [PGS] 카펫 / LV.2
    https://school.programmers.co.kr/learn/courses/30/lessons/42842

---
+++ Sjaize/Week4/카펫.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] result = new int[2];
+        int sum = (brown - 4) / 2;
+        for (int col = 1; col < sum; col++) {
+            int row = sum - col;
+            if (col * row == yellow) {
+                result[0] = row + 2;
+                result[1] = col + 2;
+                break;
+            }
+        }
+        return result;
+    }
+}
